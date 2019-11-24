1dab5833dd49706183771900e9b7aebe324cee3d
roberto <roberto.loaeza@gmail.com>
2019-11-23 21:09:35 -0600

    Fragmento Materias

--- app/src/main/java/com/mas_aplicaciones/appclass/ListadoMaterias.kt
+++ app/src/main/java/com/mas_aplicaciones/appclass/ListadoMaterias.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_listado_materias.*
 
 /**
  * A simple [Fragment] subclass.
@@ -20,5 +22,10 @@ class ListadoMaterias : Fragment() {
         return inflater.inflate(R.layout.fragment_listado_materias, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bSalir.setOnClickListener({
+            Navigation.findNavController(it).popBackStack()
+        })
+    }
 }
